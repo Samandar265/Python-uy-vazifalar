2289f21bcbca5b5b8a72b168d004b019ead8cb03
Samandar <100658893+Samandar265@users.noreply.github.com>
2022-06-04 19:37:51 +0500

    Yangilangan versia
    Xatolar to'g'irlandi

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -1,6 +1,5 @@
 import os
 import random as r
-from webbrowser import get
 
 os.system("cls")
 
@@ -13,7 +12,7 @@ def board():
 ---------------------""")
 
 def result(person,computer):
-    if (person==1 and computer==1) or (person==3 and computer==3) or (person==2 and computer==2) :
+    if person==computer:
         return 0
     elif (person==1 and computer==2) or (person==2 and computer==3) or (person==3 and computer==1):
         return -1
@@ -23,28 +22,20 @@ def result(person,computer):
 
 def result_board(person,computer,p_count,c_count):
     os.system("cls")
-    print("------------------------")
-    if person==1:
-        print("| You -> Rock          |")
-    elif person==2:
-        print("| You -> Paper         |")
-    elif person==3:
-        print("| You -> Scissors      |")
-    if computer == 1:
-        print("| Computer -> Rock     |")
-    elif computer == 2:
-        print("| Computer -> Paper    |")
-    elif computer == 3:
-        print("| Computer -> Scissors |")
+    shablon={1:"Rock",2:"Paper",3:"Scissors"}
+    print("<<<<<<<<<<<>>>>>>>>>>>")
+    print(f" You -> {shablon[person]}          ")
+
+    print(f" Computer -> {shablon[computer]} ")
 
     if result(person,computer) == -1:
-        print("|\tYou lose       |")
+        print("\tYou lose       ")
     elif result(person,computer) == 0:
-        print("|\tDraw!          |")
+        print("\tDraw!          ")
     elif result(person,computer) == 1:
-        print("|\tYou win!       |")
-    print(f"|     {p_count}    :    {c_count}      |")
-    print("------------------------")
+        print("\tYou win!       ")
+    print(f"    {p_count}    :    {c_count}      ")
+    print("<<<<<<<<<<<>>>>>>>>>>>\n")
 
 
 play=0
@@ -54,20 +45,21 @@ while person_count<3 and computer_count<3:
     board()
     person=int(input())
     if 0<person<4:
-        computer=r.randint(1,4)
-        
-        if result(person,computer) == 1:
+        computer=r.choice([1,2,3])
+        last_result=result(person,computer)
+        if last_result == 1:
             person_count+=1
-        elif result(person,computer) == -1:
+        elif last_result == -1:
             computer_count+=1
-
-        if result(person,computer) == 1 or result(person,computer) == -1:
-            play+=1
         
         result_board(person,computer,person_count,computer_count)
-        helping=input()
+        next =input("Will you continue?[y/n]")
+        if next.lower()=='n':
+            break
 os.system("cls")
-if computer_count==3:
+if computer_count>person_count:
     print("YOU ARE LOOOOSSEEEEERR!!!!")
-elif person_count==3:
+elif person_count>computer_count:
     print("You are winner!!!")
+else:
+    print("DRAW!!!!")
---
+++ test.py
@@ -0,0 +1,2 @@
+shablon={1:"Rock",2:"Paper",3:"Scissors"}
+print(shablon[3])
